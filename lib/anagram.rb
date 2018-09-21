414496836a9b15f2cb610145fe212b3ee476c3d7
jtang93 <tang.jonathan93@gmail.com>
2018-09-21 14:25:20 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(phrase)
+    phrase.to_a.select {|w| w.split("").sort==self.word.split("").sort}
+# binding.pry
+end
+
+end
