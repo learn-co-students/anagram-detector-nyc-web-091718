26e068fe367fd4a9f8a93326fb856fb604be05d2
OGonzalezNYC <OGonzalezNYC@Gmail.com>
2018-09-22 21:53:53 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,17 +1,12 @@
 # Your code goes here!
 class Anagram
   attr_accessor :word
-
   def initialize(word)
     @word = word
   end
-
-
   def match(array_of_possible_anagrams)
-    #It should return all matches in an array. If no matches exist, it should return an empty array.
-
     array_of_possible_anagrams.select do |possible_anagram|
-      @word.split("").sort == possible_anagram.split("").sort
+        @word.split("").sort == possible_anagram.split("").sort
     end
   end
 end
